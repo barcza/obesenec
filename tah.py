1951c7cec546cb045dba26a17d5920576ce530e0
Barcza Telferová <barcza@gmail.com>
2018-04-21 14:45:39 +0200

    už funguje to ověření, jestli je to jednomístné písmenko

--- tah.py
+++ tah.py
@@ -1,11 +1,24 @@
 import opratka
 
-#výjimky: ne číslo, jen len(pismeno) == 1
-
 zvolene_slovo = opratka.zvol_slovo()
+#odpoved = input("Jaké zkusíš písmeno? ")
+
+def over_delku(pismena):
+    if len(pismena) == 1:
+        return True
+    else:
+        return False
+
+def over_pismeno(cislo):
+
+    try:
+        number = int(cislo)
+    except ValueError:
+        return True
+    else:
+        return False
 
 def hledani(sluvko, pismenko):
-    #print(sluvko)
 
     if pismenko in sluvko:
         print("Ano, to tam je!")
@@ -15,6 +28,7 @@ def hledani(sluvko, pismenko):
         if sluvko.count(pismenko) > 1:
             pozice = sluvko.index(pismenko, pozice) * 2
             opratka.retezec = opratka.retezec[:pozice] + pismenko + opratka.retezec[pozice + 1:]
+            #to ale opraví jen dvě místa, nedokážu tam dostat cyklus, aby to udělalo na víc místech
 
     else:
         print("Ne, to tam není.")
@@ -24,10 +38,18 @@ def hledani(sluvko, pismenko):
     print(opratka.retezec)
 
 def hra():
+
     while "_" in opratka.retezec:
-        hledani(zvolene_slovo, input("Jaké zkusíš písmeno? "))
+
+        while True:
+            pismenko = input("Jaké zkusíš písmeno? ")
+            if over_delku(pismenko) == True and over_pismeno(pismenko) == True:
+                break
+            else:
+                print("Chci po tobě jedno písmeno")
 
 
+        hledani(zvolene_slovo, pismenko)
 
         if opratka.pocet_chyb == 10:
             print("Sorry, už visíš. Zkus to znovu.")
